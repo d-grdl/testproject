6d88f050e13c54581a59fa547f5422b72e70f94c
grdlisae <grdlisae@gmail.com>
2023-09-13 08:43:45 +0400

    House index component with search form

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import './bootstrap';
 import App from "./App.vue";
 import { createApp } from 'vue';
 import router from './router';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-const app = createApp(App).use(router);
+const app = createApp(App).use(router).use(ElementPlus);
 app.mount("#app");
--- resources/js/components/HousesIndex.vue
+++ resources/js/components/HousesIndex.vue
@@ -1,9 +1,99 @@
 <template>
-
+    <el-container>
+        <el-main>
+        <el-col style="width: 70%; margin-left: 15%">
+        <div style="width: 90%; padding-left: 2rem; padding-right: 2rem">
+            <el-form
+                label-position="top"
+                label-width="100px"
+                :model="filters"
+            >
+                <el-row :gutter="10">
+                    <el-col :xl="12" :lg="12" :md="12" :sm="12" :xs="12">
+                        <el-form-item label="Name">
+                            <el-input v-model="filters.nameQuery"
+                                      placeholder="Enter the name or part of the name..."/>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+                <el-row :gutter="10" :justify="space-between">
+                    <el-col :xl="2" :md="3" :sm="5" :xs="6">
+                        <el-form-item label="Bedrooms">
+                            <el-input v-model="filters.bedroomsNumber"/>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :xl="2" :md="3" :sm="5" :xs="6">
+                    <el-form-item label="Bathrooms">
+                        <el-input v-model="filters.bathroomsNumber"/>
+                    </el-form-item>
+                    </el-col>
+                    <el-col :xl="2" :md="3" :sm="5" :xs="6">
+                    <el-form-item label="Storeys">
+                        <el-input v-model="filters.storeysNumber"/>
+                    </el-form-item>
+                    </el-col>
+                    <el-col :xl="2" :md="3" :sm="5" :xs="6">
+                    <el-form-item label="Garages">
+                        <el-input v-model="filters.garagesNumber"/>
+                    </el-form-item>
+                    </el-col>
+                </el-row>
+                <el-row :gutter="10">
+                    <el-col :xl="6" :sm="12">
+                        <el-form-item label="Price range">
+                            <el-row :gutter="10" :justify="space-between">
+                                <el-col :xl="11" :sm="6" style="margin-bottom: 0.5rem">
+                                    <el-input v-model="filters.priceFrom" placeholder="From..." class="w-50"/>
+                                </el-col>
+                                <el-col :xl="11" :sm="6" style="margin-bottom: 0.5rem">
+                                    <el-input v-model="filters.priceTo" placeholder="To..."/>
+                                </el-col>
+                                <el-col :xl="1" :sm="1" style="margin-bottom: 0.5rem; margin-left: 2rem">
+                                    <el-form-item>
+                                        <el-button type="primary" @click="getData(this.filters)">Search</el-button>
+                                    </el-form-item>
+                                </el-col>
+                            </el-row>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+            </el-form>
+        </div>
+        <el-card class="box-card" style="width: fit-content">
+            <el-table :data="housesList" size="default" stripe>
+                <el-table-column prop="name" label="Date" sortable width="180" />
+                <el-table-column prop="bedrooms" label="Bedrooms" sortable width="130" />
+                <el-table-column prop="bathrooms" label="Bathrooms" sortable width="130" />
+                <el-table-column prop="storeys" label="Storeys" sortable width="130" />
+                <el-table-column prop="garages" label="Garages" sortable width="130" />
+                <el-table-column prop="price" label="Price" sortable width="180" />
+            </el-table>
+        </el-card>
+        </el-col>
+        </el-main>
+    </el-container>
 </template>
 
 <script>
+import { ElButton, ElTable, ElForm, ElRow, ElCol, ElCard, ElMain, ElContainer,
+    ElFormItem, ElTableColumn, ElInput} from 'element-plus'
+import { Search } from '@element-plus/icons-vue';
+
 export default {
+    components: {
+        ElButton,
+        ElTable,
+        Search,
+        ElForm,
+        ElRow,
+        ElCol,
+        ElCard,
+        ElMain,
+        ElContainer,
+        ElFormItem,
+        ElTableColumn,
+        ElInput,
+    },
     data() {
         return {
             filters: {
@@ -14,8 +104,6 @@ export default {
                 garagesNumber: null,
                 priceFrom: null,
                 priceTo: null,
-                orderBy: null,
-                asc: true,
             },
             housesList: [],
         }
@@ -29,6 +117,6 @@ export default {
     },
     mounted() {
         this.getData(this.filters);
-    }
+    },
 }
 </script>
